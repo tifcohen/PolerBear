3fab47e7f9f734bedfd55ff4b12532d46b1ac8dd
tifochen <tifcohen@gmail.com>
2016-04-22 13:14:30 +0200

    gallery view

--- app/src/main/java/com/example/tiferet/polerbear/Activities/Gallery.java
+++ app/src/main/java/com/example/tiferet/polerbear/Activities/Gallery.java
@@ -7,12 +7,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.tiferet.polerbear.Classes.Trick;
+import com.example.tiferet.polerbear.Classes.TrickDB;
+import com.example.tiferet.polerbear.Classes.User;
+import com.example.tiferet.polerbear.Classes.UserDB;
 import com.example.tiferet.polerbear.R;
 
+import java.util.List;
+
 public class Gallery extends AppCompatActivity {
+
+    List<Trick> tricks;
+    ListView trickList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,14 @@ public class Gallery extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        trickList = (ListView) findViewById(R.id.galleryList);
+
+        tricks = TrickDB.getInstance().getAllTricks();
+        //spinner.setVisibility(View.VISIBLE);
+        GalleryAdapter adapter = new GalleryAdapter();
+        trickList.setAdapter(adapter);
+        //spinner.setVisibility(View.GONE);
     }
 
     @Override
@@ -49,8 +71,49 @@ public class Gallery extends AppCompatActivity {
                 }
                 break;
         }
-
         return true;
     }
 
+    class GalleryAdapter extends BaseAdapter {
+
+        public GalleryAdapter() {
+        }
+
+        @Override
+        public int getCount() { //returns the size of the list
+            return tricks.size();
+        }
+
+        @Override
+        public Object getItem(int position) { //returns the post
+            return tricks.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) { //returns post id
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                convertView = inflater.inflate(R.layout.gallery_single_row, null);
+            }
+            final TextView trickName = (TextView) convertView.findViewById(R.id.trickName);
+            final TextView trickDate = (TextView) convertView.findViewById(R.id.trickDate);
+            /*final TextView bookReview = (TextView) convertView.findViewById(R.id.bookReview);
+            final ImageView stars = (ImageView) convertView.findViewById(R.id.stars);
+            final TextView page = (TextView) convertView.findViewById(R.id.pageTextView);
+            final TextView action = (TextView) convertView.findViewById(R.id.actionTextView);
+            final TextView action2 = (TextView) convertView.findViewById(R.id.action2TextView);
+            final ImageView userProfileImage = (ImageView) convertView.findViewById(R.id.userProfileImage);*/
+
+            Trick trick = tricks.get(position);
+            trickName.setText(trick.getTrickName());
+            trickDate.setText(trick.getTrickId()+"");
+
+            return convertView;
+        }
+    }
 }
