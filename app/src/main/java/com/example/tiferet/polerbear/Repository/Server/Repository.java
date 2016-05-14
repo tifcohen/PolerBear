97e495fb0d266b95e41fba7201c047ca6c4ac0e5
tifochen <tifcohen@gmail.com>
2016-05-15 01:47:40 +0200

    retrofit2 - is existed - full registration

--- app/src/main/java/com/example/tiferet/polerbear/Repository/Server/Repository.java
+++ app/src/main/java/com/example/tiferet/polerbear/Repository/Server/Repository.java
@@ -1,8 +1,6 @@
 package com.example.tiferet.polerbear.Repository.Server;
 
 
-import com.example.tiferet.polerbear.API.gitapi;
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,43 +8,46 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by TIFERET on 09-May-16.
  */
 public class Repository {
+    private final static Repository instance = new Repository();
     String API = "http://193.106.55.28:443/";
-    Retrofit retrofit;
+    public Retrofit retrofit;
 
     public Repository() {
-        //restAdapter.Builder() = new Retrofit().Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint(API).build();
         retrofit = new Retrofit.Builder().baseUrl(API)
                 .addConverterFactory(GsonConverterFactory.create()).build();
-
     }
 
+
+    public static Repository getInstance(){
+        return instance;
+    }
+
+/*
     public Trick foo() {
-        gitapi git = retrofit.create(gitapi.class);
-        //String user = "michael";
-        String user = "user=michael&email=michaelkolet@gmail.com&pwd=1234";
-        String email = "michaelkolet@gmail.com";
-        String pwd = "1234";
         int trickId = 1;
+        final Trick[] trick = new Trick[1];
+        ITricksAPI api = retrofit.create(ITricksAPI.class);
+        Call<Trick> call = api.getTrick(trickId);
 
-        git.getTrick(trickId);
-
-        return null;
-    }
-
-    /*
-        git.isExisted(user, email, pwd, new Callback<User>() {
+        call.enqueue(new Callback<Trick>() {
             @Override
-            public void onResponse(Call<User> call, Response<User> response) {
+            public void onResponse(Call<Trick> call, Response<Trick> response) {
                 Log.d("TAG", "pass");
+                trick[0] = response.body();
+                Log.d("TAG", ""+trick[0].getTrickName());
             }
 
             @Override
-            public void onFailure(Call<User> call, Throwable t) {
+            public void onFailure(Call<Trick> call, Throwable t) {
                 Log.d("TAG", "fail");
             }
         });
-        return null;
+
+        String user = "michael";
+        String email = "michaelkolet@gmail.com";
+        String pwd = "1234";
+
+
+        return trick[0];
     }*/
-
-
 }
