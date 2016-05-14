fc81bc2795cce6d182060dcd78465a7a91d8c546
tifochen <tifcohen@gmail.com>
2016-05-15 01:46:55 +0200

    retrofit2 - is existed - full registration

--- app/src/main/java/com/example/tiferet/polerbear/Fragments/SignUp2Fragment.java
+++ app/src/main/java/com/example/tiferet/polerbear/Fragments/SignUp2Fragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.tiferet.polerbear.R;
@@ -22,7 +23,9 @@ public class SignUp2Fragment extends Fragment {
     }
 
     public interface SignUp2FragmentDelegate{
-        void OnSignUp3();
+        void OnSignUp3(User user);
+        void OnCancel();
+
     }
 
     private SignUp2FragmentDelegate delegate;
@@ -44,17 +47,33 @@ public class SignUp2Fragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up2, container, false);
 
+        final EditText email = (EditText) view.findViewById(R.id.Join2EmailEditText);
+        EditText firstName = (EditText) view.findViewById(R.id.addUserFName);
+        EditText lastName = (EditText) view.findViewById(R.id.addUserLName);
+        final EditText birthdate = (EditText) view.findViewById(R.id.addUserBirthDate);
         final Spinner sexDropdown = (Spinner) view.findViewById(R.id.dropdown);
         final ArrayAdapter<String>sexAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, sexPickerDropdown);
         sexDropdown.setAdapter(sexAdapter);
 
         Button nextBtn = (Button) view.findViewById(R.id.nextBtn);
+        Button cancelBtn = (Button) view.findViewById(R.id.cancelBtn);
+
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (delegate != null) {
-                    delegate.OnSignUp3();
+                    user.setUserEmail(email.getText().toString());
+                    user.setUserBirthDate(birthdate.getText().toString());
+                    user.setUserSex(sexDropdown.getSelectedItem().toString());
+                    delegate.OnSignUp3(user);
                 }
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delegate.OnCancel();
             }
         });
 
