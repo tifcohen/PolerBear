9245b5fbc3e27bfc541632da543d24ac8b298dff
tifochen <tifcohen@gmail.com>
2016-05-17 00:41:28 +0200

    start of login. Might be bugs in the sign up due to changes in user class

--- app/src/main/java/com/example/tiferet/polerbear/API/IUserAPI.java
+++ app/src/main/java/com/example/tiferet/polerbear/API/IUserAPI.java
@@ -1,5 +1,7 @@
 package com.example.tiferet.polerbear.API;
 
+import com.example.tiferet.polerbear.Repository.Server.User;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -8,6 +10,10 @@ import retrofit2.http.Query;
  * Created by TIFERET on 14-May-16.
  */
 public interface IUserAPI {
+
+    //localhost:2070/Login?user={userId}&password={pwd}
+    @GET("/Login")
+    Call<User> Login(@Query("user") String user, @Query("pwd") String pwd);
     //193.106.55.28:443/isExisted?user=michael&email=michaelkolet@gmail.com&pwd=1234
     @GET("/isExisted")
     Call<Boolean> isExisted(@Query("user") String user, @Query("email") String email, @Query("pwd") String pwd);
