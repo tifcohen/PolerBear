801e656d61875a7ef7cc752622bc1ae3ff9d3a61
tifochen <tifcohen@gmail.com>
2016-08-13 16:22:17 +0300

    gallery - video

--- app/src/main/java/com/example/tiferet/polerbear/Activities/Gallery.java
+++ app/src/main/java/com/example/tiferet/polerbear/Activities/Gallery.java
@@ -1,6 +1,8 @@
 package com.example.tiferet.polerbear.Activities;
 
 import android.app.FragmentManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,10 +14,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.example.tiferet.polerbear.API.ITricksAPI;
+import com.example.tiferet.polerbear.API.IUploadFiles;
 import com.example.tiferet.polerbear.R;
 import com.example.tiferet.polerbear.Repository.Server.Repository;
 import com.example.tiferet.polerbear.Repository.Server.SessionManager;
@@ -45,6 +50,7 @@ public class Gallery extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
         final ProgressBar spinner = (ProgressBar) findViewById(R.id.spinner);
+        TextView comment = (TextView) findViewById(R.id.comment);
 
         spinner.setVisibility(View.VISIBLE);
 
@@ -110,20 +116,80 @@ public class Gallery extends AppCompatActivity {
 
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
+            final ViewHolder viewHolder;
             if (convertView == null) {
                 LayoutInflater inflater = getLayoutInflater();
                 convertView = inflater.inflate(R.layout.gallery_single_row, null);
+                viewHolder = new ViewHolder();
+                viewHolder.trickName = (TextView) convertView.findViewById(R.id.trickName);
+                viewHolder.trickDate = (TextView) convertView.findViewById(R.id.trickDate);
+                viewHolder.videoview = (VideoView) convertView.findViewById(R.id.trickVideo);
+                viewHolder.videoSpinner = (ProgressBar) convertView.findViewById(R.id.videoSpinner);
+                viewHolder.comment = (TextView) convertView.findViewById(R.id.comment);
+                convertView.setTag(viewHolder);
             }
-            final TextView trickName = (TextView) convertView.findViewById(R.id.trickName);
-            final TextView trickDate = (TextView) convertView.findViewById(R.id.trickDate);
-            final TextView comment = (TextView) convertView.findViewById(R.id.comment);
+            else{
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
 
             TrickForUser trick = tricks.get(position);
-            trickName.setText(trick.getTrickName());
-            trickDate.setText(trick.getDate());
-            comment.setText(trick.getComment());
+            viewHolder.trickName.setText(trick.getTrickName());
+            viewHolder.trickDate.setText(trick.getDate());
+            viewHolder.comment.setText(trick.getComment());
 
+            final IUploadFiles api = Repository.getInstance().retrofit.create(IUploadFiles.class);
+            final Call<String> callVideo = api.getFile(trick.getTrickPic());
+            callVideo.enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    final String VideoURL = response.body();
+                    if (VideoURL == null) {
+                        viewHolder.videoview.setVisibility(View.GONE);
+                        viewHolder.videoSpinner.setVisibility(View.GONE);
+                        return;
+                    }
+                    viewHolder.videoview.setVisibility(View.VISIBLE);
+                    try {
+                        viewHolder.videoSpinner.setVisibility(View.VISIBLE);
+                        // Start the MediaController
+                        MediaController mediacontroller = new MediaController(Gallery.this);
+                        mediacontroller.setAnchorView(viewHolder.videoview);
+                        // Get the URL from String VideoURL
+
+                        Uri video = Uri.parse(VideoURL);
+                        viewHolder.videoview.setMediaController(mediacontroller);
+                        viewHolder.videoview.setVideoURI(video);
+
+                    } catch (Exception e) {
+                        Log.e("Error", e.getMessage());
+                        e.printStackTrace();
+                    }
+
+                    viewHolder.videoview.requestFocus();
+                    viewHolder.videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        // Close the progress bar and play the video
+                        public void onPrepared(MediaPlayer mp) {
+                            //Glide.with(getActivity().getApplicationContext()).load(Uri.fromFile(new File(VideoURL))).into(thumbnail);//TODO
+                            viewHolder.videoview.pause();
+                            viewHolder.videoSpinner.setVisibility(View.GONE);
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    Log.d("show video", t.getMessage());
+                }
+            });
             return convertView;
         }
+    }
+
+    static class ViewHolder {
+        TextView trickName;
+        TextView trickDate;
+        TextView comment;
+        ProgressBar videoSpinner;
+        VideoView videoview;
     }
 }
